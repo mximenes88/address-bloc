1a674ff0fe658c77ce7ddf4989eb4af8e898508c
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2018-11-23 17:47:32 -0500

    Checkpoint 2 testing : Completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -52,4 +52,8 @@ module.exports= class MenuController {
         console.log("Thanks for using AddressBloc!");
         process.exit();
     }
+
+    getContactCount(){
+        return this.contacts.length;
+    }
 }
